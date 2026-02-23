65dd372b0f64fd3504c461bf6f8bb74d12a9bde1
yen.nguyen <yen.nguyen@digitaloutlook.com.au>
2026-02-23 22:05:37 +0700

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ThemeRegistry from '@/components/theme-registry/theme.registry';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeRegistry>
+          {children}
+        </ThemeRegistry>
       </body>
     </html>
   );
---
+++ app/playlist/page.tsx
@@ -0,0 +1,5 @@
+export default function PlaylistPage() {
+    return (
+        <div>Playlist Page</div>
+    )
+}
